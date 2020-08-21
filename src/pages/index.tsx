28a1da736034e9e265e0d85c749e7de7bd13df90
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-08-21 14:30:25 +0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontWeight: "bold" }}>coming soon</div>
+    </div>
+  )
+}
+
+export default Home
