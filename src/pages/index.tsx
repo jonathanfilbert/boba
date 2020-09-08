bc7ef4dd52631c2b4e645e65411fda4814bf2ee5
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-09-09 02:50:18 +0700

    chore: add custom self hosted fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import Layout from "../components/layout"
 
 const Home = () => {
   return (
-    <>
+    <Layout>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Jonathan Filbert - Coming Soon</title>
         <link rel="canonical" href="http://jofil.vercel.app/" />
       </Helmet>
       <div className="flex w-screen h-screen bg-black justify-center items-center">
-        <div className="uppercase text-pink-300"> coming soon</div>
+        <div className="uppercase text-pink-300 text-2xl font-black title ">
+          coming soon
+        </div>
       </div>
-    </>
+    </Layout>
   )
 }
 
