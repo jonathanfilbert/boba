9225508202813b62d8575112b6370623386a8f4b
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-09-08 23:30:04 +0700

    chore: integrate postcss and tailwind
    n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,8 @@ import React from "react"
 
 const Home = () => {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div style={{ fontWeight: "bold" }}>coming soon</div>
+    <div className="flex w-screen h-screen bg-black justify-center items-center">
+      <div className="uppercase text-pink-300"> coming soon</div>
     </div>
   )
 }
