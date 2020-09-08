12f0c1893460cc3514360562a0f277bcc493464f
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-09-08 23:50:33 +0700

    chore: add gatsby sitemap and seo components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: "https://jofil.vercel.app/",
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {},
     },
     "gatsby-plugin-postcss",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 const Home = () => {
   return (
-    <div className="flex w-screen h-screen bg-black justify-center items-center">
-      <div className="uppercase text-pink-300"> coming soon</div>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Jonathan Filbert - Coming Soon</title>
+        <link rel="canonical" href="http://jofil.vercel.app/" />
+      </Helmet>
+      <div className="flex w-screen h-screen bg-black justify-center items-center">
+        <div className="uppercase text-pink-300"> coming soon</div>
+      </div>
+    </>
   )
 }
 
