18231d1c9b4ad029fdea650dc15348161bc4f873
Jonathan Filbert <jonathan.lisyanto@bpkpenabur.sch.id>
2020-09-27 16:03:34 +0700

    fix font loading

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,29 @@ import React from "react"
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: "Work Sans";
+  src: url("/fonts/WorkSans-VariableFont_wght.ttf")
+    format("truetype");
+  font-weight: 100 800;
+}
+
+@font-face {
+  font-family: "Druk Text Wide";
+  src: url("/fonts/DrukTextWide-Heavy.woff2") format("woff2"),
+    url("/fonts/DrukTextWide-Heavy.woff") format("woff");
+  font-weight: 900;
+  font-style: normal;
+}
+
+.title {
+  font-family: "Druk Text Wide";
+}
+
+.subtitle {
+  font-family: "Work Sans";
+}
+
 `
 const Layout = ({ children }) => (
   <>
